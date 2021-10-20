e45fa6f460b0da237d6f71801ad0810e05543ce6
lunayyko <youryoung.ko@gmail.com>
2021-10-20 15:37:19 +0900

    ADD: 유닛테스트, 리드미

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # wanted x wecode
 
-- 구현한 방법과 이유에 대한 간략한 내용
-- 자세한 실행 방법(endpoint 호출방법)
-- api 명세(request/response 서술 필요)
+## 구현한 방법과 이유에 대한 간략한 내용
+
+장고와 파이썬 기술스택으로 구현했습니다.
+- bcrypt로 암호화하고, JWT를 이용해 회원가입, 로그인 구현
+- 게시글 리스트 출력(페이지네이터), 새 글 등록, 자신이 쓴 글 수정 및 삭제 구현 
+- RESTful API
+## 실행 방법(endpoint 호출방법)
+
+```python
+python manage.py runserver
+```
+해당 디렉토리에서 위의 명령어를 실행하면 localhost:8000에서 실행됩니다. 
+## api 명세(request/response 서술 필요)
+
+(get)   | /post           |  게시글 리스트 출력 | header에 토큰, body에 "text" : string
+(post)  | /post           |  새 글 등록       | header에 토큰, body에 "text" : string
+(patch) | /post/{post_id} |  글 수정
+(delete)| /post/{post_id} |  글 삭제
+
+
+
+
--- wanted/tests.py
+++ wanted/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import jwt
 
-# Create your tests here.
+from django.test                    import TestCase, Client
+from unittest.mock                  import MagicMock, patch
+
+from .models           import User, Post
+from my_settings       import SECRET_KEY
+
+class PostTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id       = 1,
+            email    = 'wecode@wanted.com',
+            password = 'wecode12#',
+        )
+        post = Post.objects.create(
+            id       = 1,
+            text     = '예쓰예쓰요',
+        )
+        post.created_at = '2021-08-19T11:03:10.216Z'
+        post.save()
+    
+    def tearDown(self):
+        Post.objects.all().delete(),
+        User.objects.all().delete(),
